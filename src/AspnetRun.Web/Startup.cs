be53d7f378b4507ae13eadd81df27d902b7227cc
RAHUL SAHU <rahulsa667@gmail.com>
2023-02-17 11:38:09 +0530

    setting updated

--- src/AspnetRun.Web/Startup.cs
+++ src/AspnetRun.Web/Startup.cs
@@ -125,12 +125,12 @@ namespace AspnetRun.Web
         public void ConfigureDatabases(IServiceCollection services)
         {
             // use in-memory database
-            services.AddDbContext<AspnetRunContext>(c =>
-                c.UseInMemoryDatabase("AspnetRunConnection"));
-
-            //// use real database
             //services.AddDbContext<AspnetRunContext>(c =>
-            //    c.UseSqlServer(Configuration.GetConnectionString("AspnetRunConnection"), x => x.MigrationsAssembly("AspnetRun.Web")));
+            //    c.UseInMemoryDatabase("AspnetRunConnection"));
+
+            // use real database
+            services.AddDbContext<AspnetRunContext>(c =>
+                c.UseSqlServer(Configuration.GetConnectionString("AspnetRunConnection"), x => x.MigrationsAssembly("AspnetRun.Web")));
         }
 
         public void ConfigureIdentity(IServiceCollection services)
