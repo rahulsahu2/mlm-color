993c96ff4f9beae75cfbfd30dbf55acf39d49649
RAHUL SAHU <rahulsa667@gmail.com>
2023-02-20 17:23:54 +0530

    new code

---
+++ src/AspnetRun.Web/Pages/dashboard.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspnetRun.Web.Pages
+{
+    public class dashboardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
